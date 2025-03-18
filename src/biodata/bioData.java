ea729ffddd8408c39fc4c29af3f2ecc884f63157
mathesh017 <sathish.cbe10480@gmail.com>
2025-03-18 10:03:53 +0530

    Update bioData.java

--- src/biodata/bioData.java
+++ src/biodata/bioData.java
@@ -2,13 +2,12 @@ package biodata;
 
 public class bioData {
 	public static void main(String[] args) {
-		System.out.println("Name : Rithiniha k ");
-		System.out.println("DOB : 15/09/2003");
-		System.out.println("Gender : Female");
-		System.out.println("Age : 21");
+		System.out.println("Name : mathesh ");
+		System.out.println("DOB : 12/10/2005");
+		System.out.println("Gender : male");
+		System.out.println("Age : 20");
 		System.out.println("Address : 03, Pennysilvenia, California");
 		System.out.println("Nationality : Indian");
 		System.out.println("Qualification : M.Sc.Software Systems");
-		System.out.println("number : +91 8886675868");
 	}
 }
